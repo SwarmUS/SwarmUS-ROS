13cdad99d0061dc047ad14c4ded80701c548b20f
e-vil <etienne.villemure@usherbrooke.ca>
2020-07-12 16:52:38 -0400

    Modified the obsolete CMakeList.txt to be able to complete the catkin_make. Changed the child link of the tf_publisher from /robot_name/base_link to /robot_name/base_footprint in order to get the transformaion of the most parent link of the robots with the world

--- swarmus_ros_simulation/src/tf_publisher.cpp
+++ swarmus_ros_simulation/src/tf_publisher.cpp
@@ -12,49 +12,26 @@ tf::Transform poseToTransform(geometry_msgs::Pose pose) {
     transform.setRotation(q);
 
     return transform;
- /*
-    static tf::TransformBroadcaster br;
-    tf::Transform transform;
-    transform.setOrigin( tf::Vector3(msg->x, msg->y, 0.0) );
-    tf::Quaternion q;
-    q.setRPY(0, 0, msg->theta);
-    transform.setRotation(q);
-    br.sendTransform(tf::StampedTransform(transform, ros::Time::now(), "world", turtle_name));
-*/
 }
- /*
-void tfPublish() {
-    static tf::TransformBroadcaster br;
-    br.sendTransform(tf::StampedTransform(transform, ros::Time::now(), "world", turtle_name));
-}*/
+
 
 void tfCallback(const gazebo_msgs::ModelStates& msg){
-    // ROS_INFO("%s", msg.name[0].c_str());
     static tf::TransformBroadcaster br;
 
     for (int i = 0; i < msg.name.size(); i++) {
+        
+        // Name of the child link: robot/base_footprint
         std::string buf(msg.name[i].c_str());
-        buf.append("/base_link");
+        buf.append("/base_footprint");
 
+        // Send transform relative to "world"
         br.sendTransform(tf::StampedTransform(
             poseToTransform(msg.pose[i]), 
             ros::Time::now(), 
             "world", 
             buf));
-        //std::map<char, int>::iterator it = m.find(msg.name[i]);
-        /*
-        if (it != tf_map.end()) { // la valeur existe
-            it->second = poseToTransform(msg.name.pose[i]);
-        } else { // miss
-            tf_map.insert({
-                msg.name[i].c_str(),
-                poseToTransform(msg.name.pose[i])
-            });
-        }*/
         // TODO delete stale robots
     }
-    
-    // tfPublish();
 }
 
 int main(int argc, char** argv){

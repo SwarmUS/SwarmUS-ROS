eb3b391bda0107a94e39fffc66ab30a978dfa13f
Etienne <vile2094@usherbrooke.ca>
2021-01-25 22:15:31 -0500

    Added some docs to the navigation.hpp

--- src/swarmus_ros_navigation/include/Navigation.hpp
+++ src/swarmus_ros_navigation/include/Navigation.hpp
@@ -21,22 +21,71 @@ struct RosParameters {
 
 class Navigation {
   protected:
+    /**
+    * @brief Gets the robot_name ROS param and modify the clientDestination accordingly
+    **/
     void fetchRosParameters();
+
+    /**
+    * @brief Struct of paramaters used by the subscriber and publisher
+    **/
     RosParameters m_RosParameters;
 
+    /**
+    * @brief Shared pointer of the NodeHandle
+    **/
     std::shared_ptr<ros::NodeHandle> m_NodeHandle;
+
+    /**
+    * @brief Subscriber used to listen to user commands
+    **/
     ros::Subscriber m_MoveBySubscriber;
+
+    /**
+    * @brief Publisher used to send goal commands to move_base
+    **/
     ros::Publisher m_GoalPublisher;
+    
+    /**
+    * @brief Current move_base goal of the robot
+    **/
     move_base_msgs::MoveBaseGoal m_CurrentGoal;
 
+    /**
+    * @brief Flag used to indicate when a new goal is generated
+    **/
     bool m_hasNewGoal;
 
+    /**
+    * @brief Transforms a x and y deplacement command from the user in a move_base goal
+    * 
+    * @details Called whenever a command is sent by the user
+    * 
+    * @param msg Message that contains a x and y deplacement command in meters
+    * 
+    **/
     void moveByCallback(const swarmus_ros_navigation::MoveByMessage& msg);
 
   public:
-    Navigation(std::shared_ptr<ros::NodeHandle> p_NodeHandle);
+    /**
+    * @brief Constructor
+    * 
+    * @param nodeHandle Shared pointer of the handle of the node
+    * 
+    **/
+    Navigation(std::shared_ptr<ros::NodeHandle> nodeHandle);
+
+    /**
+    * @brief Destructor
+    **/
     ~Navigation();
 
+    /**
+    * @brief Reads the state of the object and sends commands accordingly.
+    * 
+    * @details Verifies if a new goal has been generated and, if it's the case, publishes the goal to move_base
+    * 
+    **/
     void execute();
 };
 

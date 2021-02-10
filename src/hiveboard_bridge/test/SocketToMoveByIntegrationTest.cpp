babde3a145a702bac21ec31c9a7923958293693b
Samuel Laperriere <s.t.laperriere@gmail.com>
2021-02-10 16:46:45 -0500

    port 8080

--- src/hiveboard_bridge/test/SocketToMoveByIntegrationTest.cpp
+++ src/hiveboard_bridge/test/SocketToMoveByIntegrationTest.cpp
@@ -19,7 +19,7 @@ class TCPClient : public IProtobufStream {
         struct sockaddr_in serv_addr;
 
         serv_addr.sin_family = AF_INET;
-        serv_addr.sin_port = htons(8081);
+        serv_addr.sin_port = htons(8080);
 
         // Convert IPv4 and IPv6 addresses from text to binary form
         if (inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr) <= 0) {

534332dfe153f97ad3466fd679f7e8633710b01f
Samuel Laperriere <s.t.laperriere@gmail.com>
2021-03-18 11:49:58 -0400

    attempting to use some stuff from propolis

--- src/hive_mind_bridge/cmake/propolis.cmake
+++ src/hive_mind_bridge/cmake/propolis.cmake
@@ -11,7 +11,7 @@ function(propolis_fetch_populate)
             ${PROJECT_NAME}_propolis
 
             GIT_REPOSITORY https://github.com/SwarmUS/Propolis
-            GIT_TAG        84535d3157a3c2fe4fb2650592f03b8a2a6a8a0b
+            GIT_TAG        100de12d5f51d50857f5faa64b9b0545b9162aac
             GIT_PROGRESS   TRUE
     )
 
@@ -27,7 +27,7 @@ function(propolis_fetch_populate)
         FetchContent_Populate(${PROPOLIS})
 
         list(APPEND CMAKE_MODULE_PATH ${${PROPOLIS_L}_SOURCE_DIR}/cmake/)
-        add_subdirectory(${${PROPOLIS_L}_SOURCE_DIR}/src/pheromones ${${PROPOLIS_L}_BINARY_DIR})
+        add_subdirectory(${${PROPOLIS_L}_SOURCE_DIR}/src ${${PROPOLIS_L}_BINARY_DIR})
     endif()
 
 endfunction()

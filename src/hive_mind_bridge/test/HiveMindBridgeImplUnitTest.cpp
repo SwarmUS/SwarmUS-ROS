fd9c6fdb9d579400dec760d93e3161aad26e2b77
Samuel Laperriere <s.t.laperriere@gmail.com>
2021-03-20 14:42:06 -0400

    Fixed tests for bridgeImpl

--- src/hive_mind_bridge/test/HiveMindBridgeImplUnitTest.cpp
+++ src/hive_mind_bridge/test/HiveMindBridgeImplUnitTest.cpp
@@ -6,6 +6,17 @@
 #include "mocks/ThreadSafeQueueInterfaceMock.h"
 #include <gmock/gmock.h>
 
+std::function<std::optional<CallbackArgs>()> validCallbackWithInstantReturn = []() -> std::optional<CallbackArgs> {
+    CallbackArgs returnArgs;
+    returnArgs[0] = FunctionCallArgumentDTO(1.0f);
+
+    return returnArgs;
+};
+
+std::function<std::optional<CallbackArgs>()> validCallbackWithoutInstantReturn = []() -> std::optional<CallbackArgs> {
+    return {};
+};
+
 class HiveMindBridgeImplUnitFixture : public testing::Test {
   protected:
     TCPServerInterfaceMock m_tcpServer;
@@ -15,33 +26,51 @@ class HiveMindBridgeImplUnitFixture : public testing::Test {
     ThreadSafeQueueInterfaceMock<MessageDTO> m_queue;
     MessageHandlerInterfaceMock m_messageHandler;
 
+    MessageHandlerResult validResultWithReturn;
+    MessageDTO dummyResponseMessage = MessageUtils::createResponseMessage(1,1,1,UserCallTargetDTO::UNKNOWN,GenericResponseStatusDTO::Ok,"");
+
     void SetUp() {
         m_hivemindBridge = new HiveMindBridgeImpl(m_tcpServer, m_serializer, m_deserializer, m_messageHandler, m_queue);
+
     }
 
     void TearDown() { delete m_hivemindBridge; }
 };
 
-TEST_F(HiveMindBridgeImplUnitFixture, spinReceiveValidMessage) {
+TEST_F(HiveMindBridgeImplUnitFixture, spinInstantaneousCallback_WithReturn) {
+    // Given
+    validResultWithReturn.setFuture(std::async(std::launch::async, validCallbackWithInstantReturn).share());
+    validResultWithReturn.setResponse(dummyResponseMessage);
+    std::this_thread::sleep_for(std::chrono::milliseconds(250)); // Just to make sure the callback ends before test
+
+    // When
     EXPECT_CALL(m_tcpServer, isClientConnected()).WillOnce(testing::Return(true));
     EXPECT_CALL(m_queue, empty()).WillOnce(testing::Return(false));
     EXPECT_CALL(m_queue, front());
+    EXPECT_CALL(m_messageHandler, handleMessage(testing::_)).WillOnce(testing::Return(validResultWithReturn));
     EXPECT_CALL(m_queue, pop());
-    EXPECT_CALL(m_serializer, serializeToStream(testing::_));
+    EXPECT_CALL(m_serializer, serializeToStream(testing::_)).Times(2); // ack + return
 
     m_hivemindBridge->spin();
+
+    // Then
 }
 
-TEST_F(HiveMindBridgeImplUnitFixture, spinReceiveNoMessage) {
+TEST_F(HiveMindBridgeImplUnitFixture, spinInstantaneousCallback_WithoutReturn) {
+    // Given
+    validResultWithReturn.setFuture(std::async(std::launch::async, validCallbackWithoutInstantReturn).share());
+    validResultWithReturn.setResponse(dummyResponseMessage);
+    std::this_thread::sleep_for(std::chrono::milliseconds(250)); // Just to make sure the callback ends before test
+
+    // When
     EXPECT_CALL(m_tcpServer, isClientConnected()).WillOnce(testing::Return(true));
-    EXPECT_CALL(m_deserializer, deserializeFromStream(testing::_)).WillOnce(testing::Return(false));
+    EXPECT_CALL(m_queue, empty()).WillOnce(testing::Return(false));
+    EXPECT_CALL(m_queue, front());
+    EXPECT_CALL(m_messageHandler, handleMessage(testing::_)).WillOnce(testing::Return(validResultWithReturn));
+    EXPECT_CALL(m_queue, pop());
+    EXPECT_CALL(m_serializer, serializeToStream(testing::_)).Times(1); // ack only
 
     m_hivemindBridge->spin();
-}
 
-//TEST_F(HiveMindBridgeImplUnitFixture, spinClientDisconnected) {
-//    EXPECT_CALL(m_tcpServer, isClientConnected()).WillOnce(testing::Return(false));
-//    EXPECT_CALL(m_tcpServer, listen());
-//
-//    m_hivemindBridge->spin();
-//}
+    // Then
+}

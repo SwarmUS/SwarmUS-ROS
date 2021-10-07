df4bc94f7c72fa8de8921a7a9760a5811e1ca85b
Xavier Groleau <xavgroleau@gmail.com>
2021-10-07 10:18:41 -0400

    fix install deps

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,13 +1,12 @@
-#Get the ubuntu codename for the release
-codename=$( lsb_release --codename | cut -f2)
+#!/usr/bin/env bash
 
 # Install package to set server keys
 sudo apt-get update && sudo apt-get install software-properties-common
 
 # Register server keys to install some dependencies
 # Intel RealSense SDK
-sudo apt-key adv --keyserver keys.gnupg.net --recv-key F6E65AC044F831AC80A06380C8B3A55A6F3EFCDE || sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-key F6E65AC044F831AC80A06380C8B3A55A6F3EFCDE
-sudo add-apt-repository "deb http://realsense-hw-public.s3.amazonaws.com/Debian/apt-repo $codename main" -u
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-key F6E65AC044F831AC80A06380C8B3A55A6F3EFCDE || sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-key F6E65AC044F831AC80A06380C8B3A55A6F3EFCDE
+sudo add-apt-repository "deb https://librealsense.intel.com/Debian/apt-repo $(lsb_release -cs) main" -u
 
 # Installing dependencies
 sudo apt-get update && apt-get install -y \

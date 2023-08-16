5df7afaf58d7b0ce1fbd72bc9de25b3359f5061f
Etienne <vile2094@usherbrooke.ca>
2023-08-16 15:17:39 -0400

    - Bump Rtabmap and adapted the launchfiles accordingly - Enabled SLAM in simulation - Changed the source of odom to encoders - Added a tool to create the necessary directories for multirobot RTABMap session - Adapted tf_publisher so it could publish the transform between the world and the map whenever there is a map frame for a given robot -

--- src/swarmus_ros_simulation/include/swarmus_ros_simulation/tf_publisher.h
+++ src/swarmus_ros_simulation/include/swarmus_ros_simulation/tf_publisher.h
@@ -1,7 +1,23 @@
-#include "gazebo_msgs/ModelStates.h"
-#include "geometry_msgs/Point.h"
-#include "ros/ros.h"
 #include <map>
 #include <string>
+
+#include <gazebo_msgs/ModelStates.h>
 #include <tf/transform_broadcaster.h>
 #include <tf/transform_listener.h>
+
+class SimulationTfPublisher
+{
+    public:
+        void tfCallback(const gazebo_msgs::ModelStates& msg);
+
+    private:
+        std::map<std::string, bool> entity_publish_map_tf_list_;
+        tf::TransformBroadcaster tfBroadcaster_;
+        tf::TransformListener tfListener_;
+
+        const bool getMapToWorldTransform(const std::string& entity_name, 
+                                                                const tf::Transform& base_footprint_to_world_tf,
+                                                                tf::Transform& map_to_world_tf);
+        const tf::Transform getUnitTransform();
+
+};
